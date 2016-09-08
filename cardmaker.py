b56a124497316885eb68a8870d29cc8b08c4e051
Patrick Mullen <saluk64007@gmail.com>
2016-09-08 12:05:03 -0700

    multiple pages output separately, combined svg still available

--- cardmaker.py
+++ cardmaker.py
@@ -2,6 +2,10 @@
 #MAGIC_MED_FONT="1/8in = .125 = 9"
 #MAGIC_LARGE_FONT="3/16in = .1875 = 13.5"
 
+import os
+if not os.path.exists("output"):
+    os.mkdir("output")
+
 SMALL_FONT_SIZE=12
 MED_FONT_SIZE=15
 LARGE_FONT_SIZE=20
@@ -12,6 +16,7 @@ all_cards = []
 for sheet_name in data.sheets:
     sheet = data.sheet_by_name(sheet_name)
     all_cards.extend(pyexcel.get_records(file_name="cards.ods",sheet_name=sheet_name,name_columns_by_row=0))
+count_cards = sum([c.get("count",1) for c in all_cards])
 
 
 import svgwrite
@@ -24,17 +29,33 @@ def conv_mm(num):
 def mm(num):
     return "%dmm"%num
 
-width = conv_mm("8.5in")
-height = conv_mm("11in")
-cardsheet = svgwrite.Drawing("pirates.svg",size=("%dmm"%width,"%dmm"%height))
+cardw = 63
+cardh = 88
+spacing = 2
+pagewidth = conv_mm("8.5in")
+pageheight = conv_mm("11in")
+card_per_page = 0
+x=y=0
+while y+cardh<pageheight:
+    while x+cardw<pagewidth:
+        card_per_page += 1
+        x+=cardw+spacing 
+    x=0
+    y+=cardh+spacing
+num_pages = count_cards//card_per_page
 
-def make_img_pattern(img,width,height):
-    pattern = cardsheet.pattern(id=img,patternUnits="userSpaceOnUse",
+print(num_pages,len(all_cards),card_per_page,mm(pageheight*num_pages))
+cardsheet = svgwrite.Drawing("output/all_cards.svg",size=(mm(pagewidth),mm(pageheight*num_pages)))
+
+def make_img_pattern(root,img,width,height):
+    pattern = root.pattern(id=img,patternUnits="userSpaceOnUse",
                     width=width,height=height)
-    pattern.add(cardsheet.image("images/%s"%img,x=0,y=0,width=width,height=height))
-    cardsheet.add(pattern)
-make_img_pattern("paper_texture.jpg",600,450)
-make_img_pattern("sail_texture.jpg",253,355)
+    pattern.add(root.image("../images/%s"%img,x=0,y=0,width=width,height=height))
+    root.add(pattern)
+def init_patterns(root):
+    make_img_pattern(root,"paper_texture.jpg",600,450)
+    make_img_pattern(root,"sail_texture.jpg",253,355)
+init_patterns(cardsheet)
 
 
 def wrapped_text(text,x,y,columns,rows,vspace=5,**args):
@@ -52,50 +73,65 @@ def wrapped_text(text,x,y,columns,rows,vspace=5,**args):
         text.add(cardsheet.tspan(line,x=[x],y=[y]))
         y=mm(conv_mm(y)+vspace)
     return text
-def draw_card(card,x,y):
+def draw_card(root,card,x,y):
     if card["type"]=="pirate":
-        draw_pirate(card,x,y)
+        draw_pirate(root,card,x,y)
     if card["type"]=="skill":
-        draw_skill(card,x,y)
-def draw_pirate(card,x,y):
-    cardsheet.add(cardsheet.rect((mm(x),mm(y)),("63mm","88mm"),fill="url(#paper_texture.jpg)",stroke="black",rx=15,ry=15))
-    cardsheet.add(cardsheet.text(card["name"],x=[mm(x+63/2)],y=[mm(y+6)],text_anchor="middle",font_size=LARGE_FONT_SIZE,font_family="'Source Sans Pro', sans-serif"))
+        draw_skill(root,card,x,y)
+def draw_pirate(root,card,x,y):
+    root.add(root.rect((mm(x),mm(y)),(mm(cardw),mm(cardh)),fill="url(#paper_texture.jpg)",stroke="black",rx=15,ry=15))
+    root.add(root.text(card["name"],x=[mm(x+63/2)],y=[mm(y+6)],text_anchor="middle",font_size=LARGE_FONT_SIZE,font_family="'Source Sans Pro', sans-serif"))
     for i,trait in enumerate(card["traits"].split(",")):
-        cardsheet.add(cardsheet.text(trait,x=[mm(x+63-25)],y=[mm(y+15+i*3)],font_size=SMALL_FONT_SIZE,font_family="'Source Sans Pro', sans-serif"))
+        root.add(root.text(trait,x=[mm(x+63-25)],y=[mm(y+15+i*3)],font_size=SMALL_FONT_SIZE,font_family="'Source Sans Pro', sans-serif"))
     if "photo" in card:
-        cardsheet.add(cardsheet.image("images/"+card["photo"],x=mm(x+5),y=mm(y+7),size=(mm(28),mm(26))))
+        root.add(root.image("../images/"+card["photo"],x=mm(x+5),y=mm(y+7),size=(mm(28),mm(26))))
     if "description" in card:
-        cardsheet.add(  cardsheet.rect((mm(x+5),mm(y+53)),
+        root.add(  root.rect((mm(x+5),mm(y+53)),
                                     (mm(63-5*2),mm(30)), 
                                     fill="rgb(255,255,255)",
                                     fill_opacity=0.4)  
         )
-        cardsheet.add(wrapped_text(card["description"],x=mm(x+7),y=mm(y+57),columns=30,rows=6,font_size=MED_FONT_SIZE,font_family="'Source Sans Pro', sans-serif"))
-def draw_skill(card,x,y):
-    cardsheet.add(cardsheet.rect((mm(x),mm(y)),("63mm","88mm"),fill="url(#sail_texture.jpg)",stroke="black",rx=15,ry=15))
-    cardsheet.add(cardsheet.text(card["name"],x=[mm(x+63/2)],y=[mm(y+6)],text_anchor="middle",font_size=LARGE_FONT_SIZE,font_family="'Source Sans Pro', sans-serif"))
+        root.add(wrapped_text(card["description"],x=mm(x+7),y=mm(y+57),columns=30,rows=6,font_size=MED_FONT_SIZE,font_family="'Source Sans Pro', sans-serif"))
+def draw_skill(root,card,x,y):
+    root.add(root.rect((mm(x),mm(y)),("63mm","88mm"),fill="url(#sail_texture.jpg)",stroke="black",rx=15,ry=15))
+    root.add(root.text(card["name"],x=[mm(x+63/2)],y=[mm(y+6)],text_anchor="middle",font_size=LARGE_FONT_SIZE,font_family="'Source Sans Pro', sans-serif"))
     for i,trait in enumerate(card.get("traits","").split(",")):
-        cardsheet.add(cardsheet.text(trait,x=[mm(x+63-25)],y=[mm(y+15+i*3)],font_size=SMALL_FONT_SIZE,font_family="'Source Sans Pro', sans-serif"))
+        root.add(root.text(trait,x=[mm(x+63-25)],y=[mm(y+15+i*3)],font_size=SMALL_FONT_SIZE,font_family="'Source Sans Pro', sans-serif"))
     if "photo" in card:
-        cardsheet.add(cardsheet.image("images/"+card["photo"],x=mm(x+5),y=mm(y+7),size=(mm(28),mm(26))))
+        root.add(root.image("../images/"+card["photo"],x=mm(x+5),y=mm(y+7),size=(mm(28),mm(26))))
     if "description" in card:
-        cardsheet.add(  cardsheet.rect((mm(x+5),mm(y+53)),
+        root.add(  root.rect((mm(x+5),mm(y+53)),
                                     (mm(63-5*2),mm(30)), 
                                     fill="rgb(255,255,255)",
                                     fill_opacity=0.4)  
         )
-        cardsheet.add(wrapped_text(card["description"],x=mm(x+7),y=mm(y+57),columns=30,rows=6,font_size=MED_FONT_SIZE,font_family="'Source Sans Pro', sans-serif"))
+        root.add(wrapped_text(card["description"],x=mm(x+7),y=mm(y+57),columns=30,rows=6,font_size=MED_FONT_SIZE,font_family="'Source Sans Pro', sans-serif"))
 
 def output_cards():
+    page=0
+    drawn_sheet1 = False
+    sheet1 = svgwrite.Drawing("output/cards_page%.2d.svg"%page,size=(mm(pagewidth),mm(pageheight)))
+    init_patterns(sheet1)
     x,y=(0,0)
     for card in all_cards:
         for card_num in range(card.get("count",1)):
-            draw_card(card,x,y)
-            x+=65
-            if x+63>conv_mm("8.5in"):
+            draw_card(cardsheet,card,x,y+page*pageheight)
+            draw_card(sheet1,card,x,y)
+            drawn_sheet1 = True
+            x+=cardw+spacing
+            if x+cardw>pagewidth:
                 x=0
-                y+=90
+                y+=cardh+spacing
+                if y+cardh>pageheight:
+                    page += 1
+                    y = 0
+                    sheet1.save()
+                    drawn_sheet1 = False
+                    sheet1 = svgwrite.Drawing("output/cards_page%.2d.svg"%page,size=(mm(pagewidth),mm(pageheight)))
+                    init_patterns(sheet1)
     cardsheet.save()
+    if drawn_sheet1:
+        sheet1.save()
 
 if __name__ == "__main__":
     import sys
